2f19b605f3b0d1076798de8b5932aca24b019317
Mathew <mateusz.laskowski@hotmail.com>
2021-01-04 13:13:07 +0100

    In AddNewEventFragment.kt Adding Calendar Picker and putting it in edit text

--- app/src/main/java/com/example/happywifeapp/AddNewEventFragment.kt
+++ app/src/main/java/com/example/happywifeapp/AddNewEventFragment.kt
@@ -1,36 +1,58 @@
 package com.example.happywifeapp
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
 import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.happywifeapp.databinding.FragmentAddNewEventBinding
 import com.example.happywifeapp.databinding.FragmentUpcomingEventsBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class AddNewEventFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddNewEventBinding
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?,
     ): View {
-        val binding: FragmentAddNewEventBinding = DataBindingUtil.inflate(
+        binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_add_new_event, container, false)
 
         setupToolbar(binding)
 
+        dateSetListener = DatePickerDialog.OnDateSetListener {
+            view, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        binding.editTextDate.setOnClickListener {
+            DatePickerDialog(
+                    requireActivity(),
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
         return binding.root
 
     }
-
-
-
 
     private fun setupToolbar(binding: FragmentAddNewEventBinding) {
         val navController = findNavController()
@@ -38,4 +60,11 @@ class AddNewEventFragment : Fragment() {
         binding.toolbarAddNewEvent.title = getString(R.string.add_new_event)
         binding.toolbarAddNewEvent.setNavigationIcon(R.drawable.back_arrow_icon)
     }
+
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        binding.editTextDate.setText(sdf.format(calendar.time).toString())
+    }
+
 }
