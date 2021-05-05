c1a799baa7ccdfc0da3cf3fec3338d765f27e732
Mathew <mateusz.laskowski@hotmail.com>
2021-05-05 15:42:52 +0200

    setup fragment testing

---
+++ app/src/debug/java/com/example/happywifeapp/TestHiltActivity.kt
@@ -0,0 +1,8 @@
+package com.example.happywifeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TestHiltActivity: AppCompatActivity() {
+}
